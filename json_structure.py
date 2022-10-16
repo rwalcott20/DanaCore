20a6107615bc34eae14724e881fa737946b64587
Shonacw <shonagcw@gmail.com>
2022-10-16 16:44:55 +0100

    added schema dictionary

---
+++ json_structure.py
@@ -0,0 +1,11 @@
+schema = {
+    "taxonomy_category_name": "size",
+    "elements_in_category": [
+      {"weight": {
+        "definition": "Measurement of a specimen's weight.",
+        "data_type": "float",
+        "units": ["kg", "g", "lb"]
+      }
+      }
+    ]
+}
